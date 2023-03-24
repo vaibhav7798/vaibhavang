0a9c4f98b5edaa72829233d4a06a231bc3dc77c9
vaibhav Name <you@example.com>
2023-03-24 11:34:36 +0530

    update

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+
+   myTempRef:any;
+
+  myFunction(vaibhav:any)
+  {
+    this.myTempRef=vaibhav;
+  }
+}
